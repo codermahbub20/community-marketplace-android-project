3b9b79dd53ae3715fe3ecb07f34e386f5bcc42dc
codermahbub20 <mkrefat5@gmail.com>
2024-05-06 23:55:05 +0600

    Login Page ui design done

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,17 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image,TouchableOpacity } from 'react-native'
 import React from 'react'
-import { Image } from 'react-native-web'
+
 
 export default function LoginScreen() {
     return (
         <View>
             {/* Login Page Image Part */}
+
             <Image className="w-full h-[400px] object-cover" source={require('../../assets/images/loginA.png')}></Image>
 
             {/* Text Container */}
-            <View className="p-8">
-                <Text className="font-bold text-4xl">Community Marketplace</Text>
+            <View className="p-8 bg-white -mt-20 h-full rounded-3xl shadow-2xl ">
+                <Text className="font-bold text-center text-2xl mt-16">Community Marketplace</Text>
+                <Text className="text-md text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolor sed sint, architecto facilis voluptates odio quae alias quidem recusandae.</Text>
+
+                <TouchableOpacity onPress={()=>console.log("SignIn Now")} className="bg-blue-500 p-3 rounded-3xl mt-10">
+                    <Text  className="text-center text-white">Get Started</Text>
+                </TouchableOpacity>
             </View>
+            
 
         </View>
     )
