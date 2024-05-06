c90faa4bed505dfb6d3c1a727d8c3edacbffc37d
codermahbub20 <mkrefat5@gmail.com>
2024-05-07 00:32:38 +0600

    Cleark authentication set up done

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import LoginScreen from './Apps/Screens/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white">
-      <StatusBar style="auto" />
-      <LoginScreen />
-    </View>
+    <ClerkProvider publishableKey='pk_test_aW5zcGlyZWQtemVicmEtNjguY2xlcmsuYWNjb3VudHMuZGV2JA'>
+      <View className="flex-1 bg-white">
+        <StatusBar style="auto" />
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
