745f8da4662dedc04c1f268a06972469d9054005
codermahbub20 <mkrefat5@gmail.com>
2024-05-07 00:49:24 +0600

    Authentication part done

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,8 +1,32 @@
-import { View, Text, Image,TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
 
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } =
+                await startOAuthFlow();
+
+            if (createdSessionId) {
+                setActive({ session: createdSessionId });
+            } else {
+                // Use signIn or signUp for next steps such as MFA
+            }
+        } catch (err) {
+            console.error("OAuth error", err);
+        }
+    }, []);
+
     return (
         <View>
             {/* Login Page Image Part */}
@@ -14,11 +38,11 @@ export default function LoginScreen() {
                 <Text className="font-bold text-center text-2xl mt-16">Community Marketplace</Text>
                 <Text className="text-md text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolor sed sint, architecto facilis voluptates odio quae alias quidem recusandae.</Text>
 
-                <TouchableOpacity onPress={()=>console.log("SignIn Now")} className="bg-blue-500 p-3 rounded-3xl mt-10">
-                    <Text  className="text-center text-white">Get Started</Text>
+                <TouchableOpacity onPress={onPress} className="bg-blue-500 p-3 rounded-3xl mt-10">
+                    <Text className="text-center text-white">Get Started</Text>
                 </TouchableOpacity>
             </View>
-            
+
 
         </View>
     )
